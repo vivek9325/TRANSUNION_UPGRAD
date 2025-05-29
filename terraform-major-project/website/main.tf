9f6761882051bf31d2a6c17ffd49003f6077eaac
Vivek <vivek9325@users.noreply.github.com>
2025-05-29 19:38:37 +0530

    Update main.tf

--- terraform-major-project/website/main.tf
+++ terraform-major-project/website/main.tf
@@ -7,3 +7,29 @@ terraform {
   }
 }
 
+
+resource "google_storage_bucket" "website" {
+
+    name = "vk-29-05-2025"
+    location = "US"
+  
+}
+
+
+resource "google_storage_object_access_control" "public_rule" {
+
+    object = google_storage_bucket_object.static_site_src.name
+    bucket = google_storage_bucket.website.name
+    role = "READER"
+    entity = "allUsers"
+
+  
+}
+
+resource "google_storage_bucket_object" "static_site_src" {
+
+    name = "index.html"
+    source = "index.html"
+    bucket = google_storage_bucket.website.name
+  
+}
