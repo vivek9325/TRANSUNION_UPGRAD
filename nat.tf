eeaa3dd97004577cf92d8983421ed443d5acad14
Vivek <vivek9325@users.noreply.github.com>
2025-06-21 16:54:32 +0530

    Create nat.tf

---
+++ nat.tf
@@ -0,0 +1,12 @@
+resource "google_compute_router_nat" "mist_nat" {
+
+    name = "nat"
+    project = "upgradlabs-1746877450825"
+    router = google_compute_router.router.name
+    region = local.region
+    nat_ip_allocate_option = "AUTO_ONLY"
+    source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+    
+    depends_on = [google_compute_subnetwork.private]
+  
+}
